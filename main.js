2e6f7557b1d85091e803e97ec56f7560d28910ee
Kadir <kegilmez@gmail.com>
2019-03-29 14:32:48 +0000

    arrow buttons added to the carousel picture with event lister

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //hamburger-menu
-var iconEl = document.getElementById('hamburger-button');
+const iconEl = document.getElementById('hamburger-button');
 iconEl.addEventListener('click', () => {
     topNavEl = document.getElementById('top-nav');
     if (topNavEl.className === 'top-navigation-bar') {
@@ -9,13 +9,49 @@ iconEl.addEventListener('click', () => {
     }
 });
 
-//changing carousel picture
+//changing carousel picture with clicking the thumbnails under the picture
 //access carousel image element
-var carouselPicture = document.getElementById('carousel-picture');
+const carouselImage = document.getElementById('carousel-image');
 //access the onclick thumbnails area
-var onclickThumbnailBtn = document.querySelector('.carousel-thumbnails-wrapper');
+const onclickThumbnailBtn = document.querySelector('.carousel-thumbnails-wrapper');
 //adding event listener
 onclickThumbnailBtn.addEventListener('click', (event) => {
     //assigning carousel picture to the onclicked thumbnail
-    carouselPicture.src = event.target.src;
+    carouselImage.src = event.target.src;
 })
+
+//changing carousel picture with the 'previuos' and 'next' buttons
+//find the click buttons
+const carouselButtons = document.querySelectorAll('.carousel-button');
+carouselButtons.forEach(button => {
+    button.addEventListener('click', changePicture);
+})
+
+function changePicture(event) {
+    allThumbnails = document.querySelectorAll('.thumbnail');
+    clickedButton = event.target.innerText;
+    currentCarouselSrc = carouselImage.src;
+    if (clickedButton === '❮') {
+        allThumbnails.forEach((thumbnail, i) => {
+            if (currentCarouselSrc === thumbnail.src) {
+                if (i === 0) {
+                    previousImage = allThumbnails[allThumbnails.length - 1].src;
+                } else {
+                    previousImage = allThumbnails[i - 1].src;
+                }
+            }
+        })
+        carouselImage.src = previousImage;
+    } else {
+        allThumbnails.forEach((thumbnail, i) => {
+            if (currentCarouselSrc === thumbnail.src) {
+                if (i === allThumbnails.length - 1) {
+                    nextImage = allThumbnails[0].src;
+                } else {
+                    nextImage = allThumbnails[i + 1].src;
+                }
+            }
+        })
+        carouselImage.src = nextImage;
+    }
+}
